d49c861551643b2b412d8bd59d94936fed8f2bac
wonderit <wonsuk@broadinstitute.org>
2020-05-25 22:01:30 -0400

    fix imception score computation to accept arbitrary batch sizes

--- tflib/inception_score.py
+++ tflib/inception_score.py
@@ -54,7 +54,7 @@ def get_inception_score(images, splits=10):
         # sys.stdout.flush()
         inp = inps[(i * bs):min((i + 1) * bs, len(inps))]
         inp = np.concatenate(inp, 0)
-        pred = sess.run(softmax, {'ExpandDims:0': inp})
+        pred = sess.run(softmax, {'InputTensor:0': inp})
         preds.append(pred)
     preds = np.concatenate(preds, 0)
     scores = []
@@ -86,7 +86,12 @@ def _init_inception():
       MODEL_DIR, 'classify_image_graph_def.pb'), 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
-    _ = tf.import_graph_def(graph_def, name='')
+    # Import model with a modification in the input tensor to accept arbitrary
+    # batch size.
+    input_tensor = tf.placeholder(tf.float32, shape=[None, None, None, 3],
+                                  name='InputTensor')
+    _ = tf.import_graph_def(graph_def, name='',
+                            input_map={'ExpandDims:0': input_tensor})
   # Works with an arbitrary minibatch size.
   with tf.Session() as sess:
     pool3 = sess.graph.get_tensor_by_name('pool_3:0')
